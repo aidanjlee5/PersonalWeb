a6e33a53da622b3852df5efde9bb4ae2856ce652
Aidan <aidanjlee5@gmail.com>
2024-02-14 23:56:41 -0500

    second commit

---
+++ TechX-main/react-portfolio/src/components/About/index.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            An aspiring computer science student with interests in Web Dev, Cybersecurity, and Software Development.
+            Applying into the Computer Science school in the Spring, I hope to pursue a Bachelor's of Science in Computer Science,
+            with a minor in Data Science and Physics.
+          </p>
+          <p align="LEFT">
+            Believing in incorporating creativity and innovation into my personal projects, I strive to 
+            promote continous self-improvement through projects and internship experience 
+          </p>
+          <p>
+            When not stuck behind a computer screen or studying in the UL for 6 hours,
+            I enjoy playing Chess, Cooking, and competing in various hackathon
+
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
